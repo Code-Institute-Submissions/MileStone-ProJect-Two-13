ec64a58982182cd0a5ef42a05a607a99910865c3
romanstr-code <romanstrogoteanu1990@gmail.com>
2021-01-31 23:52:58 +0000

    Updated and changed Navbar. Hamburger to be visibile on mobile devices

--- assets/js/index.js
+++
@@ -1,4 +0,0 @@
- $(document).ready(function(){
-  $('.btn-success').tooltip({title: "Find Out more!", delay: 100}); 
-  $('.btn-danger').tooltip({title: "Find Out more!", delay: {show: 1000, hide: 100}}); 
- });

1060f977f492b5b292ffd8712644870b4798663d
romanstr-code <romanstrogoteanu1990@gmail.com>
2021-02-08 17:15:23 +0000

    Last Commit

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,14 +1,9 @@
-$(document).ready(function(){
-  $("p1").mouseenter(function(){
-    $("p1").css("background-color", "green");
-  });
-  $("p1").mouseleave(function(){
-    $("p1").css("background-color", "brown");
-  });
-});
+
+// Alert Division
 
 $(document).ready(function(){
   $(".close").click(function(){
     $("#myAlert").alert("close");
   });
 });
+
